22393e1c8fca1815b8a8b29b537159e719a77f39
Vidtion <vidtion@gmail.com>
2012-12-19 21:39:35 +0800

    update

---
+++ FindGirls/Controllers/ZJTHomeViewController_iPhone.h
@@ -0,0 +1,13 @@
+//
+//  ZJTHomeViewController_iPhone.h
+//  FindGirls
+//
+//  Created by Patrick.Z on 12/19/12.
+//  Copyright (c) 2012 ZJT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZJTHomeViewController_iPhone : UIViewController
+
+@end
---
+++ FindGirls/Controllers/ZJTHomeViewController_iPhone.m
@@ -0,0 +1,38 @@
+//
+//  ZJTHomeViewController_iPhone.m
+//  FindGirls
+//
+//  Created by Patrick.Z on 12/19/12.
+//  Copyright (c) 2012 ZJT. All rights reserved.
+//
+
+#import "ZJTHomeViewController_iPhone.h"
+
+@interface ZJTHomeViewController_iPhone ()
+
+@end
+
+@implementation ZJTHomeViewController_iPhone
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
